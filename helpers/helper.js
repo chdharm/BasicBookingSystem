660e535b1a64300739a167051413fbc9506cac58
Dharmpal <dharmpalchaudhary@airasia.com>
2019-06-17 14:16:13 +0530

    adding proper comment and date of purchase history in history.js file

--- helpers/helper.js
+++ helpers/helper.js
@@ -7,44 +7,41 @@ const session=require("../model/UserSession");
 const history=require("../model/History");
 class Helpers {
     async signup (context) {
-        
+        //Check if the same user with password and userid combination available else wirte one in Users.js/Users.txt
     }
     async login (context) {
-        // fs.readFile("", function(err, buf) {
-        //     console.log(buf);   
-        // });
-
-
+        //Validate the user by calling isValidLogIn function for normal users and isAdminLogin for admin and get the token.
+        //On open of each new page also, it should be called for validating the authenticated user
     }
     async isValidLogIn (context) {
-
+        //Valiodate the user from Users.js/Users.txt
     }
     async isAdminLogin (context) {
-
+         //Valiodate the user from Users.js/Users.txt
     }
     async logout (context) {
-
+        //Remove the cookie or token stored in local storage and refresh
     }
     async addProduct (context) {
-
+        //add product to product.js/product.txt
     }
     async updateProduct (context) {
-
+        //update product to product.js/product.txt based on product id
     }
     async deleteProduct (context) {
-
+         //delete product to product.js/product.txt based on product id
     }
     async browseProduct (context) {
-
+        //get all productrs from products.js/product.txt
     }
     async addProductToTheCart (context) {
-        
+         //add product to cart.js/cart.txt 
     }
     async getCartTotalAmount (context) {
-
+        //get all cart products for a user and get the sum
     }
     async checkout (context) {
-
+        //add the order to history
     }
 }
 module.exports = new Helpers();
--- model/History.js
+++ model/History.js
@@ -3,15 +3,16 @@ var history={
         { 
             name:"Pen",
             pid:"1",
-            price:50
+            price:50,
+            date:"02/01/2019"
         }
     ],
     12346:[
         {
             name:"Pensil",
             pid:"2",
-            price:10
-
+            price:10,
+            date:"02/01/2019"
         }
     ]
 }
